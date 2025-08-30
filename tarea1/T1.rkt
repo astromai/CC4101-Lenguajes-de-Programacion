b50a6ea9c02ea1beac2408edfa69518bcb954407
astromai <ubilla.tomas012@gmail.com>
2025-08-29 20:24:46 -0400

    feat: finish section 3

--- tarea1/T1.rkt
+++ tarea1/T1.rkt
@@ -28,7 +28,8 @@ Methodology
     (varp n)
     (andp p q)
     (orp p q)
-    (notp p))
+    (notp p)
+)
 
 #| Parte B |#
 ;; occurrences :: Prop String -> Number
@@ -45,7 +46,8 @@ Methodology
         [(orp q r) 
             (+ (occurrences q s) (occurrences r s))]
         [(notp q) 
-            (+ (occurrences q s) )]))
+            (+ (occurrences q s) )])
+)
         
 #| Parte C |#
 ;; vars :: Prop -> (Listof String)
@@ -60,7 +62,8 @@ Methodology
         [(orp q r) 
             (remove-duplicates(append (vars q) (vars r)))]
         [(notp q) 
-            (vars q)])) 
+            (vars q)])
+) 
 
 #| Parte D |#
 
@@ -72,14 +75,15 @@ Methodology
     (define x (list (cons s #t)))
     (define y (list (cons s #f)))
     (append (map (lambda (ls) (append x ls)) acc) 
-            (map (lambda (ls) (append y ls)) acc)))
-
+            (map (lambda (ls) (append y ls)) acc))
+)
 
 ;; all-environments :: (Listof String) -> (Listof (Listof (Pair String Boolean)))
 ;; Returns a list of all the possibles enviroments.
 
 (define (all-environments l) 
-    (foldr rec-enviroments '(()) l))
+    (foldr rec-enviroments '(()) l)
+)
 
 #| Parte E |#
 
@@ -112,7 +116,8 @@ Methodology
         (lambda (x acc) 
             (and acc (eval p x))) 
         #t 
-        lst))
+        lst)
+)
 
 #| P2 |#
 
@@ -137,7 +142,7 @@ Methodology
         [(notp (orp q r))
             (andp (notp (simplify-negations  q)) (notp (simplify-negations r)))]
 
-        ;; Inductiva case
+        ;; Inductive case
         [(andp q r)
             (andp (simplify-negations q) (simplify-negations r))]
         [(orp q r)
@@ -147,16 +152,57 @@ Methodology
 #| Parte B |#
 
 ;; distribute-and :: Prop -> Prop
+;; Return a proposition with transformations applied in a single pass over its conjunctions.
+
+(define (distribute-and k)
+    (match k
+        ;; Base case
+        [(varp q) (varp q)]
+        [(notp (varp q)) (notp (varp q))]
+
+        ;; Distribution
+        [(andp p (orp q r)) 
+            (orp 
+                (andp (distribute-and p) (distribute-and q)) 
+                (andp (distribute-and p) (distribute-and r)))]
+        [(andp (orp p q) r)     
+            (orp 
+                (andp (distribute-and p) (distribute-and r)) 
+                (andp (distribute-and q) (distribute-and r)))]
+
+        ;; Inductive case
+        [(notp p) 
+            (notp (distribute-and p))]
+        [(orp p q) 
+            (orp (distribute-and p) (distribute-and q))]
+        [(andp p q) 
+            (andp (distribute-and p) (distribute-and q))]))
+
 
 #| Parte C |#
 
 ;; apply-until :: (a -> a) (a a -> Boolean) -> a -> a
+;; Returns a function that repeatedly applies f until the predicate p between
+;; the last two results is true
+
+(define (apply-until f p)
+  (lambda (x)
+    (define (recurse prev)
+      (let ((curr (f prev)))
+        (if (p prev curr)
+            curr
+            (recurse curr))))
+    (recurse x)))
 
 #| Parte D |#
 
 ;; DNF :: Prop -> Prop
 
-
+(define (DNF p)
+    (define y ((apply-until simplify-negations equal?) p))
+    (define x ((apply-until distribute-and equal?) y))
+    x
+)
 
 #| P3 |#
 
@@ -164,14 +210,93 @@ Methodology
 
 ;; fold-prop :: (String -> a) (a a -> a) (a a -> a) (a -> a) -> Prop -> a
 
+(define (fold-prop f g h s)
+    (lambda (p)
+        (match p
+            [(varp q) (f q)]
+            [(andp q r) (g 
+                            ((fold-prop f g h s) q)
+                            ((fold-prop f g h s) r))]
+            [(orp q r) (h 
+                            ((fold-prop f g h s) q)
+                            ((fold-prop f g h s) r))]
+            [(notp q) (s
+                            ((fold-prop f g h s) q))]))
+)
+
 #| Parte B |#
 
 ;; occurrences-2 :: Prop String -> Number
+;; Returns the number of occurrences of a variable.
+
+(define (occurrences-2 p s)
+    ((fold-prop 
+        (lambda (v) (if (equal? v s) 1 0))
+        (lambda (v l) (+ v l))
+        (lambda (v l) (+ v l))
+        (lambda (v) v)) 
+    p)
+)
 
 ;; vars-2 :: Prop -> (Listof String)
+;; Returns a list of unique variables in the proposition.
+
+(define (vars-2 p)
+    ((fold-prop 
+        (lambda (v) (list v))
+        (lambda (v l) (remove-duplicates(append v l)))
+        (lambda (v l) (remove-duplicates(append v l)))
+        (lambda (v) v)) 
+    p)
+)
 
 ;; eval-2 :: Prop (Listof (Pair String Boolean)) -> Boolean
+;; Returns an evaluation for the proposition.
+
+(define (eval-2 p l)
+    ((fold-prop 
+        (lambda (v) (cond
+                        [(assoc v l) (cdr (assoc v l))]
+                        [else (error 'eval (format "variable ~a is not defined in environment" v))]))
+        (lambda (v t) (and v t))
+        (lambda (v t) (or v t))
+        (lambda (v) (not v))) 
+    p)
+)
 
 ;; simplify-negations-2 :: Prop -> Prop
+;; Return a simplified proposition of negations.
+
+(define (simplify-negations-2 p)
+    ((fold-prop
+        (lambda (v) (varp v))
+        (lambda (l r) (andp l r))
+        (lambda (l r) (orp l r))
+        (lambda (v)
+            (match v
+                [(notp (notp y)) y]                 
+                [(andp a b) (orp (notp a) (notp b))] 
+                [(orp a b)  (andp (notp a) (notp b))]
+                [(notp (varp x)) (varp x)] 
+                [else (notp v)]))
+                )
+    p)
+)
 
 ;; distribute-and-2 :: Prop -> Prop
+;; Return a proposition with transformations applied in a single pass over its conjunctions.
+
+(define (distribute-and-2 p)
+  ((fold-prop
+     (lambda (v) (varp v))
+     (lambda (l r)
+        (match r
+            [(orp a b) (orp (andp l a) (andp l b))]
+            [else
+                (match l
+                    [(orp a b) (orp (andp a r) (andp b r))]
+                    [else (andp l r)])]))
+     (lambda (l r) (orp l r))
+     (lambda (v) (notp v)))
+   p)
+)
--- tarea1/test.rkt
+++ tarea1/test.rkt
@@ -127,3 +127,348 @@
       (andp (varp "a") (varp "b"))
       (andp (notp (varp "b")) (varp "c"))))
   #f)    
+
+;; Test (simplify-negations)
+(test 
+  (simplify-negations
+    (notp (notp (varp "a"))))
+  (varp "a"))
+(test 
+  (simplify-negations 
+    (notp (notp (andp (varp "a") (varp "b")))))
+  (andp (varp "a") (varp "b")))  
+(test 
+  (simplify-negations
+    (notp (andp (varp "a") (varp "b"))))
+  (orp (notp (varp "a")) (notp (varp "b"))))
+(test 
+  (simplify-negations
+    (notp (orp (varp "a") (varp "b"))))
+  (andp (notp (varp "a")) (notp (varp "b"))))
+(test 
+  (simplify-negations
+    (notp (orp (notp (varp "a")) (varp "b"))))
+  (andp (notp (notp (varp "a"))) (notp (varp "b")))) 
+(test
+  (simplify-negations 
+    (andp 
+      (notp (notp (varp "a"))) 
+      (notp (notp (varp "b")))))
+  (andp 
+    (varp "a")
+    (varp "b"))) 
+
+;; Test (distribute-and)
+(test
+  (distribute-and 
+    (andp 
+      (orp (varp "a") (varp "b")) 
+      (varp "c")))
+  (orp 
+    (andp (varp "a") (varp "c")) 
+    (andp (varp "b") (varp "c"))))
+(test
+  (distribute-and 
+    (andp 
+      (varp "c") 
+      (orp (varp "a") (varp "b"))))
+  (orp 
+    (andp (varp "c") (varp "a")) 
+    (andp (varp "c") (varp "b"))))
+(test
+  (distribute-and
+    (andp 
+      (orp (varp "a") (varp "b"))
+      (orp (varp "c") (varp "d"))))
+    (orp 
+      (andp (orp (varp "a") (varp "b")) (varp "c"))
+      (andp (orp (varp "a") (varp "b")) (varp "d"))))
+(test
+  (distribute-and
+    (andp (varp "x")
+          (andp (orp (varp "y") (varp "z")) (varp "w"))))
+  (andp (varp "x")
+        (orp (andp (varp "y") (varp "w"))
+             (andp (varp "z") (varp "w")))))
+(test
+  (distribute-and
+    (andp (notp (varp "p"))
+          (orp (varp "q") (varp "r"))))
+  (orp
+    (andp (notp (varp "p")) (varp "q"))
+    (andp (notp (varp "p")) (varp "r"))))
+(test
+  (distribute-and
+    (andp (orp (varp "a") (varp "b"))
+          (andp (varp "c") (orp (varp "d") (varp "e")))))
+  (orp
+    (andp (varp "a") (orp (andp (varp "c") (varp "d")) (andp (varp "c") (varp "e"))))
+    (andp (varp "b") (orp (andp (varp "c") (varp "d")) (andp (varp "c") (varp "e"))))))
+
+;; Test (apply-until)
+(test 
+  ((apply-until
+    (\lambda (x) (/ x (add1 x)))
+    (\lambda (x new-x) (<= (- x new-x) 0.1))) 
+  1)
+  0.25)
+(test
+  ((apply-until
+     (lambda (x) (* 0.5 x))            
+     (lambda (x new-x) (<= (- x new-x) 0.05))) 
+   1)                                       
+  0.03125)                                  
+
+
+;; Test (DNF)
+(test 
+  (DNF 
+    (andp (orp (varp "a") (varp "b")) (orp (varp "c") (varp "d"))))
+  (orp 
+    (orp (andp (varp "a") (varp "c")) (andp (varp "b") (varp "c"))) 
+    (orp (andp (varp "a") (varp "d")) (andp (varp "b") (varp "d")))))
+(test
+  (DNF 
+    (andp 
+      (orp 
+        (notp (notp (varp "p"))) 
+        (varp "q"))
+      (orp 
+        (varp "r") 
+        (notp (notp (varp "s"))))))
+  (orp
+    (orp
+      (andp (varp "p") (varp "r"))
+      (andp (varp "q") (varp "r")))
+    (orp
+      (andp (varp "p") (varp "s"))
+      (andp (varp "q") (varp "s")))))
+(test
+  (DNF
+    (andp
+      (orp (notp (notp (notp (varp "p"))))
+           (orp (varp "q") (notp (notp (notp (varp "r"))))))
+      (orp (varp "x") (orp (varp "y") (varp "z")))))
+  (orp
+    (orp
+      (andp (notp (varp "p")) (varp "x"))
+      (orp
+        (andp (varp "q") (varp "x"))
+        (andp (notp (varp "r")) (varp "x"))))
+    (orp
+      (orp
+        (andp (notp (varp "p")) (varp "y"))
+        (orp
+          (andp (varp "q") (varp "y"))
+          (andp (notp (varp "r")) (varp "y"))))
+      (orp
+        (andp (notp (varp "p")) (varp "z"))
+        (orp
+          (andp (varp "q") (varp "z"))
+          (andp (notp (varp "r")) (varp "z")))))))
+
+;; Test (fold-prop)
+
+;; occurrences-2
+
+(test
+  (equal?
+    (occurrences (varp "a") "b")
+    (occurrences-2 (varp "a") "b"))
+  #t)
+(test
+  (equal?
+    (occurrences (notp (varp "a")) "b")
+    (occurrences-2 (notp (varp "a")) "b"))
+  #t)
+(test
+  (equal?
+    (occurrences (notp (varp "a")) "a")
+    (occurrences-2 (notp (varp "a")) "a"))
+  #t)
+(test
+  (equal?
+    (occurrences (andp (varp "a") (varp "b")) "a")
+    (occurrences-2 (andp (varp "a") (varp "b")) "a"))
+  #t)
+(test
+  (equal?
+    (occurrences (andp (varp "a") (varp "a")) "a")
+    (occurrences-2 (andp (varp "a") (varp "a")) "a"))
+  #t)
+(test
+  (equal?
+    (occurrences (notp (andp (varp "a") (varp "c"))) "a")
+    (occurrences-2 (notp (andp (varp "a") (varp "c"))) "a"))
+  #t)
+(test
+  (equal?
+    (occurrences (andp (varp "a") (varp "a")) "a")
+    (occurrences-2 (andp (varp "a") (varp "a")) "a"))
+  #t)
+
+;; vars-2
+(test
+  (equal?
+    (vars (varp "a"))
+    (vars-2 (varp "a")))
+  #t)
+(test
+  (equal?
+    (vars (notp (varp "a")))
+    (vars-2 (notp (varp "a"))))
+  #t)
+(test
+  (equal?
+    (vars (andp (varp "a") (varp "b")))
+    (vars-2 (andp (varp "a") (varp "b"))))
+  #t)
+(test
+  (equal?
+    (vars (andp (varp "a") (varp "a")))
+    (vars-2 (andp (varp "a") (varp "a"))))
+  #t)
+(test
+  (equal?
+    (vars (notp (andp (varp "a") (varp "c"))))
+    (vars-2 (notp (andp (varp "a") (varp "c")))))
+  #t)
+
+;; eval-2
+(test
+  (equal?
+    (eval (andp (varp "a") (varp "a")) (list (cons "a" #t)))
+    (eval-2 (andp (varp "a") (varp "a")) (list (cons "a" #t))))
+  #t)
+(test
+  (equal?
+    (eval (notp (andp (varp "a") (varp "c"))) (list (cons "a" #f) (cons "c" #f)))
+    (eval-2 (notp (andp (varp "a") (varp "c"))) (list (cons "a" #f) (cons "c" #f))))
+  #t)
+(test
+  (equal?
+    (eval (andp (varp "a") (varp "b")) (list (cons "a" #f) (cons "b" #f)))
+    (eval-2 (andp (varp "a") (varp "b")) (list (cons "a" #f) (cons "b" #f))))
+  #t)
+(test/exn
+  (eval-2 
+    (andp (varp "a") (varp "a"))
+    (list (cons "b" #t)))
+  "eval: variable a is not defined in environment")  
+
+
+;; simplify-negations-2
+(test
+  (equal?
+    (simplify-negations (notp (notp (varp "a"))))
+    (simplify-negations-2 (notp (notp (varp "a")))))
+  #t)
+(test
+  (equal?
+    (simplify-negations (simplify-negations (notp (notp (andp (varp "a") (varp "b"))))))
+    (simplify-negations-2 (simplify-negations-2 (notp (notp (andp (varp "a") (varp "b")))))))
+  #t)
+(test
+  (equal?
+    (simplify-negations (notp (andp (varp "a") (varp "b"))))
+    (simplify-negations-2 (notp (andp (varp "a") (varp "b")))))
+  #t)
+(test
+  (equal?
+    (simplify-negations (notp (orp (varp "a") (varp "b"))))
+    (simplify-negations-2 (notp (orp (varp "a") (varp "b")))))
+  #t)
+(test
+  (equal?
+    (simplify-negations (notp (orp (notp (varp "a")) (varp "b"))))
+    (simplify-negations-2 (notp (orp (notp (varp "a")) (varp "b")))))
+  #t)
+(test
+  (equal?
+    (simplify-negations (andp (notp (notp (varp "a"))) (notp (notp (varp "b")))))
+    (simplify-negations-2 (andp (notp (notp (varp "a"))) (notp (notp (varp "b"))))))
+  #t)
+
+;; distribute-and-2
+(test
+  (equal?
+    (distribute-and 
+      (andp 
+        (orp (varp "a") (varp "b")) 
+        (varp "c")))
+    (distribute-and-2 
+      (andp 
+        (orp (varp "a") (varp "b")) 
+        (varp "c"))))
+  #t)
+(test
+  (equal?
+    (distribute-and 
+      (andp 
+        (varp "c") 
+        (orp (varp "a") (varp "b"))))
+    (distribute-and-2 
+      (andp 
+        (varp "c") 
+        (orp (varp "a") (varp "b")))))
+  #t)
+(test
+  (equal?
+    (distribute-and (distribute-and
+      (andp 
+        (orp (varp "a") (varp "b"))
+        (orp (varp "c") (varp "d")))))
+    (distribute-and-2 (distribute-and-2
+      (andp 
+        (orp (varp "a") (varp "b"))
+        (orp (varp "c") (varp "d"))))))
+  #t)
+(test
+  (equal?
+    (distribute-and(distribute-and
+      (andp (varp "x")
+            (andp (orp (varp "y") (varp "z")) (varp "w")))))
+    (distribute-and-2(distribute-and-2
+      (andp (varp "x")
+            (andp (orp (varp "y") (varp "z")) (varp "w"))))))
+  #t)
+(test
+  (equal?
+    (distribute-and
+      (andp (notp (varp "p"))
+            (orp (varp "q") (varp "r"))))
+    (distribute-and-2
+      (andp (notp (varp "p"))
+            (orp (varp "q") (varp "r")))))
+  #t)
+
+;; Test particular, netamente problemas de orden de terminos pero booleanamente iguales
+#|
+=> v1
+(orp
+ (orp
+  (andp (varp "a") (andp (varp "c") (varp "d")))
+  (andp (varp "a") (andp (varp "c") (varp "e"))))
+ (orp
+  (andp (varp "b") (andp (varp "c") (varp "d")))
+  (andp (varp "b") (andp (varp "c") (varp "e")))))
+
+=>  v2
+(orp
+ (orp
+  (andp (varp "a") (andp (varp "c") (varp "d")))
+  (andp (varp "b") (andp (varp "c") (varp "d"))))
+ (orp
+  (andp (varp "a") (andp (varp "c") (varp "e")))
+  (andp (varp "b") (andp (varp "c") (varp "e")))))
+|#
+(test
+  (equal?
+    (distribute-and(distribute-and
+      (andp (orp (varp "a") (varp "b"))
+            (andp (varp "c") (orp (varp "d") (varp "e"))))))
+    (distribute-and-2(distribute-and-2
+      (andp (orp (varp "a") (varp "b"))
+            (andp (varp "c") (orp (varp "d") (varp "e")))))))
+  #f)
+
